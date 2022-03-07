ca60c22a544570095eca7a4d63129a5dfd3b416e
mtane0412 <mtane0412@gmail.com>
2022-03-08 02:16:57 +0900

    FFZだけまだうまくリストに加わってない #16

--- bttv.js
+++ bttv.js
@@ -1,16 +1,41 @@
 "use strict";
 const axios = require('axios');
 
-const bttv = (userId) => {
-    const globalUrl = 'https://api.betterttv.net/3/cached/emotes/global';
-    const bttvChannelEmotes = `https://api.betterttv.net/3/cached/users/twitch/${userId}`;
-    const ffzChannelEmotes = `https://api.betterttv.net/3/cached/frankerfacez/users/twitch/${userId}`;
-    axios.get(bttvChannelEmotes)
+const fetch = (url) => {
+    const emoteList = [];
+    return axios.get(url)
         .then(res => {
-            console.log(res.data);
+            if (url.includes('global')) {
+                res.data.forEach(emote => {
+                    emoteList.push(emote.code);
+                })
+            } else if (url.includes('frankerfacez')) {
+                emoteList.push(res.data.code);
+            } else {
+                res.data.channelEmotes.forEach(emote => {
+                    emoteList.push(emote.code);
+                })
+                res.data.sharedEmotes.forEach(emote => {
+                    emoteList.push(emote.code);
+                })
+            }
         })
         .catch(error => console.log(error))
-        .then(()=> console.log('end'));
+        .then(() => { return emoteList })
 }
 
-bttv(195327703);
+const getAllEmotes = (userId) => {
+    const bttvGlobalEmotes = `https://api.betterttv.net/3/cached/emotes/global`;
+    const bttvChannelEmotes = `https://api.betterttv.net/3/cached/users/twitch/${userId}`;
+    const ffzChannelEmotes = `https://api.betterttv.net/3/cached/frankerfacez/users/twitch/${userId}`;
+    return Promise.all([fetch(bttvGlobalEmotes), fetch(bttvChannelEmotes), fetch(ffzChannelEmotes)])
+}
+
+
+const bttv = async (userId) => {
+    const emoteLists = await getAllEmotes(userId);
+    const emoteList = emoteLists.flat();
+    return emoteList
+}
+
+module.exports = bttv;
