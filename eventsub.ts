a0813fe8949378109c5b3fb05afb01767d452deb
mtane0412 <mtane0412@gmail.com>
2022-03-11 22:54:24 +0900

    お掃除

--- eventsub.ts
+++ eventsub.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 import { ClientCredentialsAuthProvider } from '@twurple/auth';
-import { ApiClient, HelixEventSubApi } from '@twurple/api';
+import { ApiClient, HelixEventSubSubscription, HelixPaginatedResultWithTotal } from '@twurple/api';
 import { EventSubListener } from '@twurple/eventsub';
 import { NgrokAdapter } from '@twurple/eventsub-ngrok';
 dotenv.config();
@@ -12,8 +12,6 @@ const clientSecret: string = process.env.CLIENT_SECRET;
 const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
 const apiClient = new ApiClient({ authProvider });
 
-const helixEvent = new HelixEventSubApi(apiClient);
-
 const listener = new EventSubListener({
 	apiClient,
 	adapter: new NgrokAdapter(),
@@ -21,47 +19,42 @@ const listener = new EventSubListener({
 });
 
 const userId: string = '195327703';
-/*
-const onlineSubscription = await listener.subscribeToStreamOnlineEvents(userId, e => {
-    console.log(`${e.broadcasterDisplayName} just went live!`);
-});
 
-const offlineSubscription = await listener.subscribeToStreamOfflineEvents(userId, e => {
-    console.log(`${e.broadcasterDisplayName} just went offline`);
-});
-*/
-
-const clearSubscriptions = ():void => {
-    /* EventSubをすべてunsubscribeする */
-    helixEvent.getSubscriptions().then(res=> {
-        res.data.forEach((sub)=>{
-            sub.unsubscribe();
-            console.log(`${sub.id} is unsubscribed`);
-        })
-    });
+const getSubscriptions = ():Promise<HelixPaginatedResultWithTotal<HelixEventSubSubscription>> => {
+    return apiClient.eventSub.getSubscriptions();
 }
 
-const channelPointListen = () => {
+const deleteAllSubscriptions = async ():Promise<void> => {
+    apiClient.eventSub.deleteAllSubscriptions();
+    console.log('deleted all subscriptions.')
+}
+
+const subscribeToChannelCheerEvents = async ():Promise<void> => {
+    listener.subscribeToChannelCheerEvents(userId, e=> {
+        console.log(`${e.bits} bits by ${e.userName}`);
+    })
+}
+const subscribeToChannelRedemptionAddEvents = async():Promise<void> => {
     listener.subscribeToChannelRedemptionAddEvents(userId, e => {
         console.log(`${e.redeemedAt}: ${e.userName} ${e.broadcasterName} ${e.rewardTitle}`);
     });
 }
 
-const followerListen = () => {
+const subscribeToChannelFollowEvents = async():Promise<void> => {
     listener.subscribeToChannelFollowEvents(userId, e => {
-        console.log(`${e.userDisplayName ? e.userDisplayName : e.userName}さん、フォローありがとうございます！ Thanks for the follow!`);
+        console.log(`${e.userDisplayName ? e.userDisplayName : e.userName}`);
     })
 }
 
-(async () => {
+(async ():Promise<void> => {
     // 実行時にとにかくEventSub全部消す
-    clearSubscriptions();
-    helixEvent.getSubscriptions().then(res=> {
-        if (res.total === 0) {
-            channelPointListen();
-            followerListen();
-            listener.listen();
-            console.log('listening...');
-        }
-    });
+    await deleteAllSubscriptions();
+    // console.log()
+    await subscribeToChannelCheerEvents();
+    await subscribeToChannelRedemptionAddEvents();
+    await subscribeToChannelFollowEvents();
+    const subscriptions = await getSubscriptions();
+    console.log(subscriptions);
+    listener.listen();
+    console.log('listening...');
 })();
